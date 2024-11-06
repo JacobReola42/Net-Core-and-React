b76923791b12d347fa4780d820f4fa3134480aa8
Jacob Reola <jacobreola@gmail.com>
2024-11-06 16:29:45 -0600

    fetching data without cors

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -1,9 +1,26 @@
+/* 26 */
+
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/activities").then((response) => {
+      setActivities(response.data);
+    });
+  }, []); // For dependencies. Empty means called once i.e. []
+
   return (
     <>
       <h1>Reactivities</h1>
+      <ul>
+        {activities.map((activity: any) => (
+          <li key={activity.id}>{activity.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
