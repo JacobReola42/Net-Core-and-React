367226f10b0fba2ee6d7d9d637362ffb769018a0
Jacob Reola <jacobreola@gmail.com>
2024-11-06 20:18:12 -0600

    updated with semanti ui

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
+import { Header, List } from "semantic-ui-react";
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -16,12 +17,12 @@ function App() {
 
   return (
     <>
-      <h1>Reactivities</h1>
-      <ul>
+      <Header as="h2" icon="users" content="Reactivities" />
+      <List>
         {activities.map((activity: any) => (
-          <li key={activity.id}>{activity.title}</li>
+          <List.Item key={activity.id}>{activity.title}</List.Item>
         ))}
-      </ul>
+      </List>
     </>
   );
 }
