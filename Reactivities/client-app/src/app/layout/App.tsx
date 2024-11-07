d57895ef45f58ad691712adde540e37777400d6d
Jacob Reola <jacobreola@gmail.com>
2024-11-07 11:58:00 -0600

    M48 Navbar styling

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -1,8 +1,8 @@
 /* 26 */
 
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import axios from "axios";
-import { Header, List } from "semantic-ui-react";
+import { Container, Header, List } from "semantic-ui-react";
 import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
 
@@ -19,14 +19,20 @@ function App() {
   // For dependencies. Empty means called once i.e. []
 
   return (
+    // Fragment removes the useless div from html output to the browser 48.
+    // Fragment helps keep the Navbar and container return same level and not 2 different elements.
+    // <Fragment> is optional vs <></>
     <>
       <NavBar />
-      <List>
-        {activities.map((activity) => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
+      <Container style={{ marginTop: "7em" }}>
+        <List>
+          {activities.map((activity) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+      </Container>
     </>
+    // </Fragment>
   );
 }
 
